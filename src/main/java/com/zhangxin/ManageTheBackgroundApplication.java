014f6bd5902b636751aacd9f6a285b82c04383dd
zhangxin <zhangxin1233>
2017-11-22 17:33:58 +0800

    新增管理后台

---
+++ src/main/java/com/zhangxin/ManageTheBackgroundApplication.java
@@ -0,0 +1,12 @@
+package com.zhangxin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManageTheBackgroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManageTheBackgroundApplication.class, args);
+	}
+}
